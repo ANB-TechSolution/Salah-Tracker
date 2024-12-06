500f883cd5aeb658255f735090f3bfda0b68545f
Rony Boay <ronyboay@gmail.com>
2024-12-07 01:46:40 +0500

    updatedn

---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/home/haroon/snap/flutter/common/flutter"
+export "FLUTTER_APPLICATION_PATH=/media/haroon/New Volume/a_flutter_projects/basics_Flutter/SalahTracker/salahtracker/build/web/Salah-Tracker"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
